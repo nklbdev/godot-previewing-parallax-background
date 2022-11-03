6e3a4097c19c01d4ce542743774973a56da69a7d
nklbdev <nklbdev@gmail.com>
2022-11-04 01:16:38 +0500

    fix paths

--- addons/nklbdev.previewing_parallax_background/plugin_manager.gd
+++ addons/nklbdev.previewing_parallax_background/plugin_manager.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 const _ppb_class_name: String = "PreviewingParallaxBackground"
 const _ppb_base_class_name: String = "ParallaxBackground"
-const _ppb_script: GDScript = preload("PreviewingParallaxBackground.gd")
-const _icon_texture: Texture = preload("type_icon.svg")
+const _ppb_script: GDScript = preload("res://addons/nklbdev.previewing_parallax_background/PreviewingParallaxBackground.gd")
+const _icon_texture: Texture = preload("res://addons/nklbdev.previewing_parallax_background/type_icon.svg")
 
 func _enter_tree():
 	add_custom_type(_ppb_class_name, _ppb_base_class_name, _ppb_script, _icon_texture)

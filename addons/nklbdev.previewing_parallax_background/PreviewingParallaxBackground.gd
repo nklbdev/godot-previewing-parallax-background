2f010a4e4d7ac6659e354271b3ae89d6c4e4786c
nklbdev <nklbdev@gmail.com>
2022-11-05 21:26:31 +0500

    fix getting scene editor viewport and icon path

--- addons/nklbdev.previewing_parallax_background/PreviewingParallaxBackground.gd
+++ addons/nklbdev.previewing_parallax_background/PreviewingParallaxBackground.gd
@@ -1,7 +1,8 @@
 tool
 extends ParallaxBackground
+class_name PreviewingParallaxBackground
 
-onready var _editor_viewport: Viewport = _find_editor_viewport(get_tree().root) if Engine.editor_hint else null
+var _editor_viewport: Viewport
 var _previous_viewport_transform: Transform2D
 export var preview_enabled: bool = true setget _set_preview_enabled
 func _set_preview_enabled(value: bool):
@@ -13,6 +14,12 @@ func _set_preview_enabled(value: bool):
 			_update_children(true)
 		else:
 			_reset_children()
+
+func _ready():
+	if Engine.editor_hint:
+		var edited_scene_root = get_tree().edited_scene_root
+		if edited_scene_root:
+			_editor_viewport = edited_scene_root.get_viewport()
 
 func _enter_tree():
 	# reset positions that was changed in editor and saved in scene
@@ -38,15 +45,3 @@ func _update_children(force: bool = false):
 func _process(_delta):
 	if preview_enabled and Engine.editor_hint and _editor_viewport:
 		_update_children()
-
-func _find_editor_viewport(node: Node, recursive_level = 0) -> Viewport:
-	if node.get_class() == "CanvasItemEditor":
-		return node.get_child(1).get_child(0).get_child(0).get_child(0).get_child(0) as Viewport
-	recursive_level += 1
-	if recursive_level > 15:
-		return null
-	for child in node.get_children():
-		var result = _find_editor_viewport(child, recursive_level)
-		if result:
-			return result
-	return null

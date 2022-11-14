5edcea5de985ef14f82e60cf019d37e0601ce2a8
nklbdev <nklbdev@gmail.com>
2022-11-14 12:46:57 +0500

    port original behavior

--- addons/nklbdev.previewing_parallax_background/PreviewingParallaxBackground.gd
+++ addons/nklbdev.previewing_parallax_background/PreviewingParallaxBackground.gd
@@ -3,7 +3,7 @@ extends ParallaxBackground
 class_name PreviewingParallaxBackground
 
 var _editor_viewport: Viewport
-var _previous_viewport_transform: Transform2D
+var _previous_canvas_transform: Transform2D
 export var preview_enabled: bool = true setget _set_preview_enabled
 func _set_preview_enabled(value: bool):
 	if preview_enabled == value:
@@ -31,17 +31,39 @@ func _reset_children():
 		if child is ParallaxLayer:
 			child.position = Vector2.ZERO
 
-func _update_children(force: bool = false):
-	var viewport_transform = _editor_viewport.global_canvas_transform
-	if not force and viewport_transform == _previous_viewport_transform:
-		return
-	_previous_viewport_transform = viewport_transform
-	var visibleRectGlobal: Rect2 = viewport_transform.affine_inverse().xform(Rect2(Vector2.ZERO, _editor_viewport.size))
-	var view_center = visibleRectGlobal.position + visibleRectGlobal.size / 2
-	for child in get_children():
-		if child is ParallaxLayer:
-			child.position = view_center - view_center * child.motion_scale + child.motion_offset
-
 func _process(_delta):
 	if preview_enabled and Engine.editor_hint and _editor_viewport:
 		_update_children()
+
+func _update_children(force: bool = false):
+	var canvas_transform = _editor_viewport.global_canvas_transform
+	if not force and canvas_transform == _previous_canvas_transform:
+		return
+	_previous_canvas_transform = canvas_transform
+	
+	var vps = _editor_viewport.size
+	var inverted_canvas_transform = canvas_transform.affine_inverse()
+	var screen_offset = -inverted_canvas_transform.xform(vps / 2)
+
+	var scroll_ofs = scroll_base_offset + screen_offset * scroll_base_scale
+	
+	scroll_ofs = -scroll_ofs
+	if scroll_limit_begin.x < scroll_limit_end.x:
+		if scroll_ofs.x < scroll_limit_begin.x:
+			scroll_ofs.x = scroll_limit_begin.x
+		elif scroll_ofs.x + vps.x > scroll_limit_end.x:
+			scroll_ofs.x = scroll_limit_end.x - vps.x
+
+	if scroll_limit_begin.y < scroll_limit_end.y:
+		if scroll_ofs.y < scroll_limit_begin.y:
+			scroll_ofs.y = scroll_limit_begin.y
+		elif scroll_ofs.y + vps.y > scroll_limit_end.y:
+			scroll_ofs.y = scroll_limit_end.y - vps.y
+	scroll_ofs = -scroll_ofs;
+	
+	var scroll_scale = inverted_canvas_transform.get_scale().dot(Vector2.ONE)
+	scroll_ofs = (scroll_ofs + screen_offset * (scroll_scale - 1)) / scroll_scale
+
+	for child in get_children():
+		if child is ParallaxLayer:
+			child.position = scroll_ofs * child.motion_scale + child.motion_offset - screen_offset
